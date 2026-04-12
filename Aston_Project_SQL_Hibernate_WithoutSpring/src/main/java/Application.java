01f6cc4d3e19a4ce18f54cc639d132d233a4001c
YuliyaVasilenko <153922208+YuliyaVasilenko@users.noreply.github.com>
2026-04-13 00:22:21 +0300

    Delete Aston_Project_SQL_Hibernate_WithoutSpring directory

--- Aston_Project_SQL_Hibernate_WithoutSpring/src/main/java/Application.java
+++
@@ -1,9 +0,0 @@
-import console.ConsoleUI;
-
-public class Application {
-    public static void main(String[] args) {
-        ConsoleUI consoleUI = new ConsoleUI();
-        consoleUI.start();
-    }
-}
-
